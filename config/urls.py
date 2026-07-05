ac7b72347ed13ac4c7c566c289891d9ad63959a9
AnkanDebnath2 <ankandebnath700@gmail.com>
2026-07-05 20:07:05 +0600

    No  commit

---
+++ config/urls.py
@@ -0,0 +1,13 @@
+
+
+
+from django.contrib import admin
+from django.urls import path
+
+from flatax.views import index, contact
+
+urlpatterns = [
+   path('', index, name='index'),
+   path('contact/', contact, name='contact'),
+   path('admin/', admin.site.urls),
+]
