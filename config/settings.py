522df50200374616c643e208089d1d16553cbff0
AnkanDebnath2 <ankandebnath700@gmail.com>
2026-07-13 01:40:39 +0600

    Items added

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'flatax',
+    'items',
+    
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -116,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
+
--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,11 @@
 
 
 
+
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from flatax.views import index, contact
 
@@ -11,3 +14,6 @@ urlpatterns = [
    path('contact/', contact, name='contact'),
    path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
