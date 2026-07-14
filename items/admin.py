a2b3c0aa1c9b6a099d5c87e05e9e79c6b4d94b2c
AnkanDebnath2 <ankandebnath700@gmail.com>
2026-07-15 02:10:40 +0600

    Addad multiple image

--- items/admin.py
+++ items/admin.py
@@ -1,7 +1,14 @@
 
 
 from django.contrib import admin
-from .models import Item, Category
+from .models import Item, Category, ItemImage
 
-admin.site.register(Item)
+class ItemImageInline(admin.TabularInline):
+    model = ItemImage
+    extra = 10  
+
+class ItemAdmin(admin.ModelAdmin):
+    inlines = [ItemImageInline]
+
+admin.site.register(Item, ItemAdmin)
 admin.site.register(Category)
--- items/models.py
+++ items/models.py
@@ -17,16 +17,29 @@ class Item(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
 
+    Location= models.TextField(blank=True, null=True)
+
     is_sold = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='items_images', blank=True, null=True)
+   
 
     "image = models.ImageField(upload_to='items_images', blank=True, null=True)"
 
 
     def __str__(self):
         return self.name
+    
+
+
+class ItemImage(models.Model):
+    item = models.ForeignKey(Item, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='items/')
+
+    def __str__(self):
+        return f"Image for {self.item.name}"     
+   
 
 
 
